947337ad891fc27201aa6c86e54e1250005bb432
Daniel DeFrance <daniel.defrance@montana.edu>
2022-10-26 15:41:41 -0600

    changed port number

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5022
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!')
+  res.send('Dan here. Good to meet you!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
